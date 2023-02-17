e33420a823e0094ccceda3630a08337da1c28edd
Hope-Ray <90702054+Hope-Ray@users.noreply.github.com>
2023-02-17 06:11:04 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import cors from "cors"
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30MB", extended: "true"}));
+app.use(bodyParser.urlencoded({ limit: "30MB", extended: "true"}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
+
+//const CONNECTION_URL = "mongodb+srv://anurag123:anurag123@cluster0.dcw3ieo.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery',false);
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
